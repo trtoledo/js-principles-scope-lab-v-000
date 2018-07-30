0bd3fac09be4b022afba7fafbe0dee0011344599
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-30 20:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var customerName = "bob"
 function upperCaseCustomerName(){
 return customerName = customerName.toUpperCase();
 }
+let bestCustomer = "not bob"
 
 
 // Write your solution in this file!
