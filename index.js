5afdce06f92f324d1b6522e8232519f2a3c24f56
Tomas Toledo <trtoledo1977@gmail.com>
2018-07-30 19:52:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var customerName = bob
-function upperCaseCustomerName(){
-  return customerName
-}
+//function upperCaseCustomerName(){
+  //return customerName
+//}
 
 
 // Write your solution in this file!
